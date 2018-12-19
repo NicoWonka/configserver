7ec6e2021ef30fbc5e51df0b98bf72a833b60410
Nicolas Ponce <nponce@apiservice.cl>
2018-12-19 17:05:08 -0300

    Se actualiza version spring boot y se modifican properties, para levantamiento en docker.

--- src/main/java/cl/apiservice/ConfigApplication.java
+++ src/main/java/cl/apiservice/ConfigApplication.java
@@ -3,12 +3,13 @@ package cl.apiservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 @EnableConfigServer
 public class ConfigApplication{
 	
